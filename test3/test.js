dae8e1549b0a06e72b608a815a1567a16187bf3f
Antonio Pacheco <antonio_pacheco@outlook.com>
2016-08-29 21:33:30 -0700

    jsonp plus ajax

--- test3/test.js
+++ test3/test.js
@@ -1,22 +1,38 @@
-var getEmployees = function(){
-	return(
-		$.ajax({
-		    url: 'https://www.foxnotes.com/sfiles/restsamples.nsf/employees?readform',
-		    type: 'GET',
-		    crossDomain: true,
-		    success: function(data){ 
-		        console.log(data);
-		    },
-		    error: function(x,y,z) {
-		        
-		    }
-		})
-	);
+var getEmployeesbyGet = function(){
+
+	 $.ajax({
+        type: "post",
+        url: "https://www.foxnotes.com/sfiles/restsamples.nsf/employees?readform",
+        dataType:"json",
+        success: function (response) {
+        	console.log(response);
+            // if(response.status === "success") {
+            //     console.log();
+            // } else if(response.status === "error") {
+            //     // do something with response.message or whatever other data on error
+            // }
+        },
+        error: function(response){
+        	console.log(response);
+        }
+    })
 }
 
+var getEmployeesbyJsonP = function(){
+
+	$.ajax({
+		url: "https://www.foxnotes.com/sfiles/restsamples.nsf/employees?readform",
+		dataType: "jsonp",
+		timeout: 5000,
+		success: function (parsed_json) {
+		console.log(parsed_json);
+		},
+		error: function (xhr, textStatus, errorThrown) {
+		console.log(xhr,textStatus,errorThrown);     
+		}
+	});
+}
 $(function() {
-	var employes = getEmployees();
-	$.when( employes} ).done(function( x ) {
- 		 alert( x.testing ); // Alerts "123"
-});
+	getEmployeesbyGet();
+	getEmployeesbyJsonP();
 });
